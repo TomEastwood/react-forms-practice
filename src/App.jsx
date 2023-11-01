f0b5e1cb72ffd80c5705f71dbd1c14f1933ba0f1
TomEastwood <t.eastwood99@gmail.com>
2023-11-01 12:36:59 +0000

    practice completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,82 @@
 import { useState } from "react";
 import "./App.css";
 
+const initialFormState = {
+  name: "",
+  address: "",
+  phone: "",
+  email: "",
+  complaint: "",
+  contact: "",
+  consent: false,
+};
+
 export default function App() {
 
   //TODO: Add your state fields here
 
+  const [formState, setFormState] = useState(initialFormState);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formState);
+    setFormState(initialFormState);
+  }
+  
+
+  const handleChange = (event) => {
+    const { name, type, value, checked } = event.target;
+    if (type === "checkbox") {
+      setFormState({ ...formState, [name]: checked });
+    } else {
+      setFormState({ ...formState, [name]: value });
+    }
+  };
+
+  const contactOptions = ["phone", "email", "post", "none"]
+
   return (
     <>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h2>Complaining form!</h2>
         <div className="form__section-left">
           <label>
             Full name
-            <input type="text" name="name" required />
+            <input 
+            onChange={(e) => handleChange(e)}
+            value={formState.name}
+            type="text" 
+            name="name" 
+            required 
+            />
           </label>
           <label>
             Address
-            <input type="text" name="address" />
+            <input 
+            onChange={(e) => handleChange(e)}
+            value={formState.address}
+            type="text" 
+            name="address" 
+            />
           </label>
           <label>
             Phone Number
-            <input type="tel" name="phone" />
+            <input 
+            onChange={(e) => handleChange(e)}
+            value={formState.phone}
+            type="tel" 
+            name="phone" 
+            />
           </label>
 
           <label>
             Email
-            <input type="email" name="email" />
+            <input 
+            onChange={(e) => handleChange(e)}
+            value={formState.email}
+            type="email" 
+            name="email" 
+            />
           </label>
         </div>
 
@@ -33,6 +84,8 @@ export default function App() {
           <label>
             Write your complaint
             <textarea
+              onChange={(e) => handleChange(e)}
+              value={formState.complaint}
               name="complaint"
               rows="10"
               placeholder="You can complain here"
@@ -41,30 +94,29 @@ export default function App() {
 
           <div className="form__radio-group">
             <p>How do you want to be contacted? </p>
-            <label>
-              <input type="radio" name="contact" value="phone" />
-              Phone
-            </label>
-
-            <label>
-              <input type="radio" name="contact" value="email" />
-              Email
-            </label>
-
-            <label>
-              <input type="radio" name="contact" value="post" />
-              Slow Mail
-            </label>
-
-            <label>
-              <input type="radio" name="contact" value="none" />
-              No contact!
-            </label>
+            {contactOptions.map((option) => 
+              <label key={option}>
+                {option}
+                <input
+                  onChange={(e) => handleChange(e)}
+                  checked={formState.contact === option} 
+                  type="radio" 
+                  name="contact" 
+                  value={option} 
+                />
+              </label>
+            )}
           </div>
 
           <label>
             I agree you take my data, and do whatever
-            <input type="checkbox" name="consent" id="consent" />
+            <input 
+            onChange={(e) => handleChange(e)}
+            checked={formState.consent}
+            type="checkbox" 
+            name="consent" 
+            id="consent" 
+            />
           </label>
         </div>
         <input type="submit" value="Submit!" />
